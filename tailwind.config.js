cfa6d7cb03776d211c5e8fd1606112993c04b24b
biplab-mistry <kr.biplabmistry@gmail.com>
2025-01-29 15:19:19 +0530

    navbar and hero section done

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins"', "sans-serif"],
+        inter: ['"Inter"', "sans-serif"],
+      },
+      colors: {
+        header: '#000000',
+        buttonBg: '#E76F51',
+        subHeader: '#0A0826',
+        paragraph: '#6C6C6C',
+        white: '#FFFFFF',
+        secondaryHeader: '#18191F',
+      },
+    },
+  },
+  // Other Tailwind configuration settings
+};
